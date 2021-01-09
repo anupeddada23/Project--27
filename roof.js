e0727d1ec39d376388d02523c2e901d8f7970fb8
anupeddada23 <71734343+anupeddada23@users.noreply.github.com>
2021-01-09 11:48:28 -0600

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,18 @@
+  
+class Roof{
+    constructor(){
+        var options={
+            isStatic:true
+        }
+        this.body= Bodies.rectangle(250,100,400,25,options)
+        this.width=400
+        this.height=25
+        World.add(world,this.body)
+    }
+
+    display()
+    {
+		fill("black")
+        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+    }
+}
